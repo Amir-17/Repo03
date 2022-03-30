a398c61bfa67243f1ec7bbf3914a53e20fa7a6d5
Amir-17 <amirkahriman17@gmail.com>
2022-03-30 13:10:10 +0200

    Adding ex02,03,04,05,06

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function.main() {
+    // Only change code below this line
+    // JS comment - single line
+    /* 
+    JS comment - this code have
+    multiline comment
+    */
+    // Only change code aboce this line
+}
+console.log(main());
+module.exports = main;
